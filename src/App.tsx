96e6c369555a51f560b67a54672e04445a7103ba
zeik <alexgermosen@gmail.com>
2025-04-09 22:21:57 -0400

    realizacion de asignacion react contador

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { count } from 'console';
 
 function App() {
+
+  const [nombre, setNombre] = useState('');
+  const [mostrarMensaje, setMostrarMensaje] = useState(false);
+  const [contador, setContador] = useState(0);
+  const [timer, setTimer] = useState(0);
+
+  const manejarContador = (count:any) => {
+    setNombre(count.target.value);
+    setMostrarMensaje(true);
+  };
+
+  const mostrarSaludo = () => {
+    setMostrarMensaje(true);
+    setTimer(0);
+  };
+
+  useEffect(() => {
+      const timerID = setInterval(() => setTimer((t) => t+1), 1000);
+      return () => clearInterval(timerID);
+
+  }, []);
+
+  useEffect(() => {
+    console.log(`Cambio el contador: ${contador}`);
+
+  }, [contador]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CONTADOR REACT</h1>
       </header>
+
+      <div>
+        <input
+          type="text"
+          value={nombre}
+          onChange={manejarContador}
+        />
+        <button onClick={mostrarSaludo}>Mostrar saludo!</button>
+
+        {mostrarMensaje && (
+          <p>{nombre}, tiempo transcurrido: {timer} segundos desde el ultimo saludo.</p>
+        )}
+
+        <h2>Contador: {contador}</h2>
+        <button onClick={() => setContador(contador + 1)}>Aumentar</button>
+        <button onClick={() => setContador(contador - 1)}>Decrementar</button>  
+      </div>
     </div>
   );
 }
